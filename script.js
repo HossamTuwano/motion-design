0defd6f10afe81d595ca7d989ba7664ed01da001
HossamTuwano <hossamtuwano@gmail.com>
2023-10-21 07:30:05 +0300

    staging final

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-$("body").addClass("playing");
-
-   
+$(window).load(function () {
+  setTimeout(function () {
+    // use .addClass() and .removeClass() to swap the loading class for loaded on #stage.
+    $('#stage').removeClass('.loading').addClass('loaded')
+  }, 3000);
+});
