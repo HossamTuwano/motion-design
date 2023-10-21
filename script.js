da15c978d3f32944b901e4800998384837f75636
HossamTuwano <hossamtuwano@gmail.com>
2023-10-21 20:38:52 +0300

    final aliceandtuna

--- script.js
+++ script.js
@@ -1,6 +1,45 @@
 $(window).load(function () {
-  setTimeout(function () {
-    // use .addClass() and .removeClass() to swap the loading class for loaded on #stage.
-    $('#stage').removeClass('.loading').addClass('loaded')
-  }, 3000);
+  //Start up the skrollr object
+  skrollr.init({
+    //keeps the bottom from being long
+    forceHeight: false,
+  });
+
+  // The rest of this controls switching Alice's moods using jQuery Waypoints
+
+  // How to guess which frame is "being read".
+  var beingRead = function () {
+    // It would be approximately centered, of equal distance from top as from bottom.
+    var $screenHeight = $.waypoints("viewportHeight");
+    var $pageHeight = $(".page").height();
+    var offset = (($pageHeight - $screenHeight) / 2) * -1;
+
+    return offset;
+  };
+
+  $(".page").waypoint(
+    function (direction) {
+      var mood = $(this).data("mood");
+      $("body").removeClass().addClass(mood);
+      if (direction === "up") {
+        // if scrolling up
+        $(this)
+          .addClass("in-view")
+          .removeClass("scrolled-past")
+          .waypoint("next")
+          .removeClass("in-view");
+      } else {
+        // else, assuming we're not scrolling at all or are scrolling down
+        $(this)
+          .addClass("in-view")
+          .removeClass("scrolled-past")
+          .waypoint("prev")
+          .removeClass("in-view")
+          .addClass("scrolled-past");
+      }
+    },
+    {
+      offset: beingRead(),
+    }
+  );
 });
